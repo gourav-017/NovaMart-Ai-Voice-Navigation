ba25b6dc8090d1731d333904008ec0364cabf6db
gourav-017 <gouravrajput23110@gmail.com>
2025-12-01 23:35:00 +0530

    commit

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -35,6 +35,7 @@ function Nav() {
         try {
             const result = await axios.get(serverUrl + "/api/auth/logout" , {withCredentials:true})
             console.log(result.data)
+            getCurrentUser(null);
             navigate("/login")
         } catch (error) {
             console.log(error)
