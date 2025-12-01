77736afd90f7134f978139c8227d788b4e22ab39
gourav-017 <gouravrajput23110@gmail.com>
2025-12-01 20:10:27 +0530

    commit

--- backend/Server.js
+++ backend/Server.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:'*',
+ origin:["http://localhost:5173","http://localhost:5154"],
  credentials:true
 }))
 
